744d10a914bc1713267d811d34bc322d6700b73f
vale9746 <82171057+vale9746@users.noreply.github.com>
2021-04-20 16:22:15 +0200

    Classe rete aggiornata, Gestione file aggiornata

--- ProgettoIngeSW/src/MainProgram/Rete.java
+++ ProgettoIngeSW/src/MainProgram/Rete.java
@@ -16,7 +16,9 @@ public class Rete {
 	ArrayList<RelazioneDiFlusso> relazioni;
 	@XmlTransient
 	int [][] in;
+	int [][]out;
 	
+	//costruttore
 	public Rete (){
 		numPos=0;
 		numTrans=0;
@@ -30,10 +32,12 @@ public class Rete {
         return this.relazioni;
     }
 	
+	//aggiunge relazioni di flusso 
 	public void aggiungiRelazione(RelazioneDiFlusso r) {
 		this.getRelazioni().add(r);
 	}
 	
+	//ritorna il numero massimo delle transizioni 
 	public void contaTransizioni() {
 		int max=0;
 		for(RelazioneDiFlusso r : relazioni) {
@@ -44,6 +48,7 @@ public class Rete {
 		System.out.println("numero transizioni" + max);
 	}
 	
+	//ritorna il numero massimo della posizione
 	public void contaPosizioni() {
 		int max=0;
 		for(RelazioneDiFlusso r : relazioni) {
@@ -54,6 +59,7 @@ public class Rete {
 		System.out.println("numero posizioni" + max);
 	}
 	
+	//stampa le matrici: debug
 	private void stampaMatrice(int [][]m) {
 		for (int i=0; i<numPos; i++) {
 			for(int j=0; j<numTrans; j++) {
@@ -63,28 +69,31 @@ public class Rete {
 		}
 	}
 	
+	//inizializza tutta la rete 
 	public void inizializzaRete() {
 		contaPosizioni();
 		contaTransizioni();
 		in = new int [numPos][numTrans];
-		
+		out = new int [numPos][numTrans];
 		for (int i=0; i<numPos; i++)
 		{
 			for(int j=0; j<numTrans; j++)
 			{
 				for(RelazioneDiFlusso r: relazioni)
 				{
-					if(r.getPosizione()==i+1 && r.getTransizione()==j+1 && r.getInOut())
-						in[i][j]=1; 
+					if(r.getPosizione()==i+1 && r.getTransizione()==j+1 && r.getInOut()==true) 
+						in[i][j]=1; 	
+					else if (r.getPosizione()==i+1 && r.getTransizione()==j+1 && r.getInOut()==false)
+						out[i][j]=1;
 				}
-				
-				System.out.printf("%d", in[i][j]);
 			}
-			System.out.println();
 		}
+		//debug
+		stampaMatrice(in);
+		System.out.println();
+		stampaMatrice(out);
+
 	}
-	
-	
 	
 	
 	public void stampaRete() {
