38d09ec31bdf3ca156ba390aa24c56ffdade882a
mikcrd <71429562+mikcrd@users.noreply.github.com>
2021-04-27 16:26:35 +0200

    modificata classe Menu

--- ProgettoIngeSW/src/MainProgram/Menu.java
+++ ProgettoIngeSW/src/MainProgram/Menu.java
@@ -15,7 +15,6 @@ public class Menu {
 	public void cicloApplicazione() {
 		
 		MyMenu menu = new MyMenu(TITOLO,MENU);
-  		menu.stampaMenu();
   		
   		ArchivioReti archivio = new ArchivioReti();
   		if(file.length() != 0L) {
@@ -23,18 +22,19 @@ public class Menu {
   		}
   		
   		do{
-			
-   		   int cmd = menu.scegli();
-   		   switch(cmd)
-   		   {
-   		       case 0: System.exit(0); break;
-   		       case 1: archivio.aggiungiRete(); break;
-   		       case 2: archivio.visualizzaRete(); break;
-   		       case 3: archivio.eliminaRete(); break;
-   		       case 4: archivio.visualizzaArchivio(); break;
-   		       
-   		   }
-  		
+  			
+	  		   menu.stampaMenu();
+	   		   int cmd = menu.scegli();
+	   		   switch(cmd)
+	   		   {
+	   		       case 0: System.exit(0); break;
+	   		       case 1: archivio.aggiungiRete(); break;
+	   		       case 2: archivio.visualizzaRete(); break;
+	   		       case 3: archivio.eliminaRete(); break;
+	   		       case 4: archivio.visualizzaArchivio(); break;
+	   		       
+	   		   }
+	  		
   		}while(true);
 	
 	}
