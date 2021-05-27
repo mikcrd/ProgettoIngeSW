f59e03ad93586406dcd7b968ce6f4a9f52942d82
vale9746 <82171057+vale9746@users.noreply.github.com>
2021-05-27 11:07:45 +0200

    corretto AbstractRete

--- Vers2_ProgettoIngeSW/src/MainProgram2/ArchivioReti.java
+++ Vers2_ProgettoIngeSW/src/MainProgram2/ArchivioReti.java
@@ -1,5 +1,7 @@
 package MainProgram2;
 
+
+
 import java.util.ArrayList;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -10,12 +12,32 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
+import utility.LeggiInput;
+
+
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
     "reti"
 })
 @XmlRootElement(name = "archivioReti")
 public class ArchivioReti {
+	
+	private final static String MESS_NOME = "Inserisci il nome della rete da aggiungere: ";
+	private final static String MESS_DOPPIONE = "Attenzione: non si puo' inserire una rete  gia' esistente!";
+	private final static String MESS_NOME_GIA_PRESENTE = "Nell'archivio è già presente una "
+			+ "rete con questo nome. Inserire un altro nome: ";
+	private final static String MESS_CERCA_RETE = "Inserisci il nome della rete: ";
+	private final static String MESS_RIMOZIONE = " : confermi la rimozione di questa rete?";
+	private final static String MESS_NON_TROVATA = "Rete richiesta non trovata";
+	
+	private static final String INSERIMENTO_RELAZIONI = "Vuoi inserire un'altra relazione?";
+	private static final String POSTOTRANS_TRANSPOSTO = "Per aggiungere una coppia posto-transizione premere 'a'\n"
+			+ "Per aggiungere una coppia transizione-posto premere 'b' : ";
+	private static final String POSTO = "Inserisci un intero positivo per il posto: ";
+	private static final String TRANSIZIONE = "Inserisci un intero positivo per la transizione: ";
+	private static final String ERRORE_SCELTA_AB = "Inserisci solo i caratteri 'a' o 'b' : ";
+	private static final String NOME_RETE_VISUALIZZA = "Inserisci il nome della rete da visualizzare: ";
+	private static final String ERRORE_ARCHIVIO_VUOTO = "Attenzione archivio vuoto";
 
 		@XmlElementWrapper(name= "reti")
 		@XmlElement(name="rete", required = true)
@@ -35,6 +57,42 @@ public class ArchivioReti {
 				reti = new ArrayList<AbstractRete>();
 	        }
 	        return this.reti;
+		}
+		
+		public AbstractRete trovaRete (String reteRichiesta)
+		{ 
+			for(AbstractRete elem : reti){	  
+				if(elem.getName().equalsIgnoreCase(reteRichiesta))
+					return elem;
+			} 
+			return null;  
+		 }
+		
+		public AbstractRete cercaRete()
+		{
+			String net = LeggiInput.leggiStringaNonVuota(MESS_CERCA_RETE);
+			return trovaRete(net);
+		}
+		
+		public void eliminaRete()
+		{
+			AbstractRete elemento = cercaRete();
+			if (elemento!= null)
+			{
+				boolean procedi = LeggiInput.yesOrNo(elemento.getName() + MESS_RIMOZIONE);
+					if (procedi)
+						reti.remove(elemento);
+			}
+			else {
+				LeggiInput.leggiStringa(MESS_NON_TROVATA);
+			}
+		}
+		
+		public void aggiungiRete(AbstractRete r) {
+			if (r instanceof Rete)
+				
+			else if (r instanceof RetePN)
+				
 		}
 	
 		
